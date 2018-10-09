1872bf3ee2c3abf38f22623129d9934babeb4d3a
bryan <bryanw83@yahoo.com>
2018-10-09 14:41:16 -0400

    Initial commit

---
+++ src/HelloWorld/HelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World");
+		
+		System.out.println("Here's more stuff");
+
+	}
+
+}
